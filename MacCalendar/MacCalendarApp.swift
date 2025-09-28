9e9da33d5186e28e4841490dd49706a3305f79d9
叉叉 <ruihe.lin@live.com>
2025-09-28 17:06:37 +0800

    Initial Commit

---
+++ MacCalendar/MacCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacCalendarApp.swift
+//  MacCalendar
+//
+//  Created by ruihelin on 2025/9/28.
+//
+
+import SwiftUI
+
+@main
+struct MacCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
