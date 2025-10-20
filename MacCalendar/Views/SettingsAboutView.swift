5901874108e06b564add307bfce46a0c0f990419
Pingfan Hu <pingfan0727@gmail.com>
2025-10-20 19:50:03 -0400

    Update app icons and about view, fix bundle ID
    Updated all AppIcon assets with new images and added a new icon.png. Changed the PRODUCT_BUNDLE_IDENTIFIER to 'com.pingfan.maccalendar' in the Xcode project. Improved the SettingsAboutView with a new GitHub link, updated the URL, and added hover effects for the GitHub logo.

--- MacCalendar/Views/SettingsAboutView.swift
+++ MacCalendar/Views/SettingsAboutView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct SettingsAboutView: View {
+    @State private var isHoveringGitHub = false
+
     var body: some View {
         VStack(alignment:.center,spacing: 12){
             Text("MacCalendar")
@@ -24,15 +26,25 @@ struct SettingsAboutView: View {
             }
             .font(.customSize(14))
             .foregroundStyle(.secondary)
-            
-            Link(destination: URL(string:"https://github.com/bylinxx/MacCalendar")!) {
+
+            Link(destination: URL(string:"https://github.com/pingfan-hu/MacCalendar")!) {
                 Image("github-logo")
                     .resizable()
                     .scaledToFit()
                     .frame(width: 55, height: 55)
                     .clipShape(Circle())
+                    .scaleEffect(isHoveringGitHub ? 1.1 : 1.0)
+                    .animation(.easeInOut(duration: 0.2), value: isHoveringGitHub)
             }
-            
+            .onHover { hovering in
+                isHoveringGitHub = hovering
+                if hovering {
+                    NSCursor.pointingHand.push()
+                } else {
+                    NSCursor.pop()
+                }
+            }
+
             Spacer()
         }
         .frame(maxWidth: .infinity)
